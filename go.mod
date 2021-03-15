04cc8cd1cc9f4e5e443a17c70a68ae42729bfd46
Dvortsova Varvara <vvdvortsova@edu.hse.ru>
2021-03-15 18:24:03 +0300

    init commit

---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/vvdvortsova/greetings
+
+go 1.15
+
---
+++ greetings.go
@@ -0,0 +1,11 @@
+package greetings
+
+import "fmt"
+import gd "example.com/greetingsdeps"
+
+// Hello returns a greeting for the named person.
+func Hello(name string) string {
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Hi, %v. %v!", name, gd.Greeting())
+    return message
+}
